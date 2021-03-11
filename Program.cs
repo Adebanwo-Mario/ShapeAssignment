e75a327c4ca46ec9d278ecaa8cdfddd0e316cdb0
Adebanwo Uthman <78296221+Akinde-Omogbolahan@users.noreply.github.com>
2021-03-11 19:27:46 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Shape>();
+
+            list.Add(new Rectangle(4, 7));
+            list.Add(new Circle(8));
+            list.Add(new Triangle(5, 10));
+
+
+
+            Console.WriteLine($"Area of Rectangle: {list[0].CalculateSurface()}");
+            Console.WriteLine($"Area of Circle: {list[1].CalculateSurface()}");
+            Console.WriteLine($"Area of Triangle: {list[2].CalculateSurface()}");
+            Console.ReadKey();
+        }
+    }
+}
